8d14dea7cb08d02fc3d0bd202a0fac78ce8f236d
ryuhyeji <hyeji5207@gmail.com>
2023-08-30 11:15:03 +0900

    first

---
+++ src/app/components/footer/Footer.js
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return(
+        <div style={{width:'80%', margin:'auto', padding:'20px', textAlign:'center', backgroundColor:"lightgray"}}>
+            Copyright &copy; 2023 한국 ICT인재개발원
+        </div>
+    );
+
+}
